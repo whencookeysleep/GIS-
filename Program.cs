1975fff025ae8771f762cc9ce415dfed8f5eba83
whencookeysleep <154298279+whencookeysleep@users.noreply.github.com>
2023-12-19 12:07:12 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using ESRI.ArcGIS.esriSystem;
+
+namespace GIS应用系统
+{
+    static class Program
+    {
+       
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.EngineOrDesktop);//添加许可
+            IAoInitialize _IAoInitialize = new AoInitializeClass();
+            _IAoInitialize.Initialize(esriLicenseProductCode.esriLicenseProductCodeEngine);
+            _IAoInitialize.CheckOutExtension(esriLicenseExtensionCode.esriLicenseExtensionCodeSpatialAnalyst);
+            Application.Run(new frmMain());         
+        }
+    }
+}
